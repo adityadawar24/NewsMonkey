bdf2c88ebb92e590f8117d2483b62f6579c4e97f
aditya24 <adityadawar1999@gmail.com>
2023-06-13 19:38:38 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## News Monkey
+News Monkey is a single-page application that allows users to stay up-to-date on the latest news from various sources. The application uses the News API to fetch the latest news articles, which can be filtered by category to help users find the news that's most relevant to them.  
+
+### Installation
+To run the application locally, you'll need to have Node.js installed on your machine. Make sure to get a API key from news api. Create a new .env file and paste it there REACT_APP_NEWS_KEY="api_key". Once you've cloned the repository, navigate to the project directory in your terminal and run the following commands:
+
+```
+npm install
+npm start
+```
+
+### Usage
+Once the application is running, you can use the search bar to find news articles based on keywords, or use the navbar to filter news by category. The application also features infinite scrolling, so you can continue to load more articles as you scroll down the page.
+
+### Resources
+React.js  
+Bootstrap  
+News API
---
+++ src/App.js
@@ -0,0 +1,133 @@
+import "./App.css";
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import { Routes, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
+
+const App=()=>{
+  const apiKey="b5fc48ca02b24cd58a5cea6d9d0fe325";
+  const pageSize=10; 
+
+  const[progress,setProgress]=useState(0)
+
+  const changeProgress=((progress)=>{
+    setProgress(progress)
+  })
+
+    return (
+      <div>
+        <LoadingBar color="#f11946" progress={progress} />
+        <Navbar />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <News
+                apiKey={apiKey}
+                key={"general"}
+                setProgress={changeProgress}
+                pageSize={pageSize}
+                country={"us"}
+                title="Home Page"
+                category={"general"}
+              />
+            }
+          ></Route>
+          <Route
+            exact
+            path="/business"
+            element={
+              <News
+                apiKey={apiKey}
+                key={"business"}
+                setProgress={changeProgress}
+                pageSize={pageSize}
+                country={"us"}
+                title="Business News"
+                category={"business"}
+              />
+            }
+          ></Route>
+          <Route
+            exact
+            path="/entertainment"
+            element={
+              <News
+                apiKey={apiKey}
+                setProgress={changeProgress}
+                key={"entertainment"}
+                pageSize={pageSize}
+                country={"us"}
+                title="Entertainment News"
+                category={"entertainment"}
+              />
+            }
+          ></Route>
+          <Route
+            exact
+            path="/health"
+            element={
+              <News
+                apiKey={apiKey}
+                setProgress={changeProgress}
+                key={"health"}
+                pageSize={pageSize}
+                country={"us"}
+                title="Health News"
+                category={"health"}
+              />
+            }
+          ></Route>
+          <Route
+            exact
+            path="/science"
+            element={
+              <News
+                apiKey={apiKey}
+                setProgress={changeProgress}
+                key={"science"}
+                pageSize={pageSize}
+                country={"us"}
+                title="Science News"
+                category={"science"}
+              />
+            }
+          ></Route>
+          <Route
+            exact
+            path="/sports"
+            element={
+              <News
+                apiKey={apiKey}
+                setProgress={changeProgress}
+                key={"sports"}
+                pageSize={pageSize}
+                country={"us"}
+                title="Sports News"
+                category={"sports"}
+              />
+            }
+          ></Route>
+          <Route
+            exact
+            path="/technology"
+            element={
+              <News
+                apiKey={apiKey}
+                setProgress={changeProgress}
+                key={"technology"}
+                pageSize={pageSize}
+                country={"us"}
+                title="Technology News"
+                category={"technology"}
+              />
+            }
+          ></Route>
+        </Routes>
+      </div>
+    );
+  }
+
+  export default App;
